7f24a6b2bbbfa7d787b7be7e7065d03a9e5db2d8
curtdragon <tealturboser@gmail.com>
2019-08-14 19:25:41 -0500

    Added different port

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+require("dotenv").config();
 const server = require('./api/server.js');
-
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
+//When running locally the IP is taken care of by the localhost
+//and we can hardcode any old port overr 3000
+
+// when deploying
+
+//Heroku will assign an IP address
+//We can't hardcode in the port
+//Hero needs to tell us which port because it might be taken
